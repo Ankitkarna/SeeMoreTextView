c0fdf62a70f5c69144c256b7785681939ce21636
Ankit <ankit.karna2011@gmail.com>
2020-04-27 21:14:23 +0545

    handled case for seemore text length same as original text length

--- SeeMoreTextView/ViewController.swift
+++ SeeMoreTextView/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         setupNavigationBar()
     }
     
+    let otherText = "One\nTwo\nThree\nFour"
     let shortText = "Hi there testing header view \nWith \n\nNew something"
     let longText = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
     
@@ -46,7 +47,7 @@ class ViewController: UIViewController {
     
     @objc private func nextButtonTapped() {
         let controller = ExampleTableController()
-        controller.text = shortText
+        controller.text = otherText
         navigationController?.pushViewController(controller, animated: true)
     }
 
